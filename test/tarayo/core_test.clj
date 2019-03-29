40becb4ab6e2ad24767107b6e3fb7755fc5b029d
liquidz <liquidz.uo@gmail.com>
2019-03-29 18:03:56 +0900

    Add tests for tarayo.core

--- test/tarayo/core_test.clj
+++ test/tarayo/core_test.clj
@@ -1,4 +1,15 @@
 (ns tarayo.core-test
   (:require [clojure.test :as t]
-            [tarayo.core :as sut]))
+            [tarayo.core :as sut]
+            [tarayo.test-helper :as h]))
 
+(t/deftest connect-test
+  (h/with-test-smtp-server [srv port]
+    (let [test-message {:from "alice@example.com" :to "bob@example.com"
+                        :subject "hello" :body "world"}]
+      (t/is (empty? (h/get-received-emails srv)))
+
+      (with-open [conn (sut/connect {:port port})]
+        (t/is (= {:result :success} (sut/send! conn test-message))))
+
+      (t/is (= [test-message] (h/get-received-emails srv))))))
