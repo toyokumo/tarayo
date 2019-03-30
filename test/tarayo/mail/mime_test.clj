2531c8a0643f0da8532f093b4bab7fb672474f65
liquidz <liquidz.uo@gmail.com>
2019-03-30 19:36:17 +0900

    Add tests for mail.mime

--- test/tarayo/mail/mime_test.clj
+++ test/tarayo/mail/mime_test.clj
@@ -3,10 +3,30 @@
             [clojure.test :as t]
             [tarayo.mail.mime :as sut]
             [tarayo.test-helper :as h])
-  (:import javax.mail.internet.MimeMessage))
+  (:import [javax.mail.internet MimeMessage MimeMultipart]))
 
 (t/deftest make-message-test
   (let [{:keys [session]} (h/test-connection)
-        msg (sut/make-message session {:from "foo" :to "bar" :subject "hello" :body "world" :charset "UTF-8"})]
+        opts {:from "foo" :to "bar" :subject "hello" :body "world" :charset "UTF-8"}
+        msg (sut/make-message session opts)]
     (t/is (instance? MimeMessage msg))
-    (t/is (str/includes? (.getMessageID msg) "@tarayo"))))
+    (t/is (str/includes? (.getMessageID msg) "@tarayo"))
+    (t/is (str/starts-with? (first (.getHeader msg "User-Agent")) "tarayo/"))
+    (t/is (= "world" ^String (.getContent msg)))))
+
+(t/deftest make-message-with-custom-user-agent-test
+  (let [{:keys [session]} (h/test-connection)
+        opts {:from "foo" :to "bar" :subject "hello" :body "world" :charset "UTF-8"
+              :user-agent "FooBar"}
+        msg (sut/make-message session opts)]
+    (t/is (instance? MimeMessage msg))
+    (t/is (= ["FooBar"] (seq (.getHeader msg "User-Agent"))))))
+
+(t/deftest make-message-with-sequential-bodies-test
+  (let [{:keys [session]} (h/test-connection)
+        opts {:from "foo" :to "bar" :subject "hello" :charset "UTF-8"
+              :body [{:type "text/plain" :content "world"}
+                     {:type "text/html" :content "<p>world</p>"}]}
+        msg (sut/make-message session opts)]
+    (t/is (instance? MimeMessage msg))
+    (t/is (instance? MimeMultipart (.getContent msg)))))
