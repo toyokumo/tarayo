23508b6d4ba4b8be2d4b5a6d8286139fc2e05f74
liquidz <liquidz.uo@gmail.com>
2019-03-31 10:07:51 +0900

    Add tests for mime

--- test/tarayo/mail/mime_test.clj
+++ test/tarayo/mail/mime_test.clj
@@ -2,8 +2,9 @@
   (:require [clojure.string :as str]
             [clojure.test :as t]
             [tarayo.mail.mime :as sut]
+            [tarayo.mail.mime.message :as message]
             [tarayo.test-helper :as h])
-  (:import [javax.mail.internet MimeMessage MimeMultipart]))
+  (:import [javax.mail.internet InternetAddress MimeMessage MimeMultipart]))
 
 (t/deftest make-message-test
   (let [{:keys [session]} (h/test-connection)
@@ -13,6 +14,19 @@
     (t/is (str/includes? (.getMessageID msg) "@tarayo"))
     (t/is (h/tarayo-user-agent? (first (.getHeader msg "User-Agent"))))
     (t/is (= "world" ^String (.getContent msg)))))
+
+(t/deftest make-message-with-cc-bcc-test
+  (let [{:keys [session]} (h/test-connection)
+        opts {:from "foo" :to "bar" :subject "hello" :body "world" :charset "UTF-8"
+              :cc "alice" :bcc "bob"}
+        msg (sut/make-message session opts)]
+    (t/is (instance? MimeMessage msg))
+
+    (let [ccs (.getRecipients msg message/recipient-type-cc)
+          bccs (.getRecipients msg message/recipient-type-bcc)]
+      (t/is (= 1 (count ccs) (count bccs)))
+      (t/is (= "alice" (.getAddress ^InternetAddress (first ccs))))
+      (t/is (= "bob" (.getAddress ^InternetAddress (first bccs)))))))
 
 (t/deftest make-message-with-custom-user-agent-test
   (let [{:keys [session]} (h/test-connection)
