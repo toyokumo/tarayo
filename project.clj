336115971d6f33f38701e31716e659a2047e7803
liquidz <liquidz.uo@gmail.com>
2019-09-26 16:09:23 +0900

    Bump libraries

--- project.clj
+++ project.clj
@@ -4,28 +4,30 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [commons-codec "1.12"]
-                 [com.sun.mail/jakarta.mail "1.6.3"]
-                 [jakarta.mail/jakarta.mail-api "1.6.3"]
-                 [nano-id "0.9.3"]
-                 [org.apache.tika/tika-core "1.20"]]
+                 [commons-codec "1.14"]
+                 [com.sun.mail/jakarta.mail "1.6.4"]
+                 [jakarta.mail/jakarta.mail-api "1.6.4"]
+                 [nano-id "0.10.0"]
+                 [org.apache.tika/tika-core "1.23"]
+                 [camel-snake-kebab "0.4.1"]]
 
   :profiles
   {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
                         [fudje "0.9.7"]
                         [orchestra "2019.02.06-1"]
                         [com.github.kirviq/dumbster "1.7.1"]
+
                         ;; for benchmark
-                        [criterium "0.4.4"]
+                        [criterium "0.4.5"]
                         [com.draines/postal "2.0.3"]]
          :source-paths ["dev/src" "src"]
          :resource-paths ["dev/resources"]
          :global-vars {*warn-on-reflection* true}}
 
    :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
-   :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
-   :it [:dev {:dependencies [[org.clojure/data.json "0.2.6"]
-                             [camel-snake-kebab "0.4.0"]
+   :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}
+   :it [:dev {:dependencies [[org.clojure/data.json "1.0.0"]
+                             [camel-snake-kebab "0.4.1"]
                              [http-kit "2.3.0"]]
               :test-paths ["integration/test"]}]}
   :aliases
@@ -33,7 +35,7 @@
    "test-whale" ["with-profile" "1.9,it:1.10,it" "test"]
    "benchmark" ["run" "-m" "benchmark"]}
 
-  :plugins [[lein-cloverage "1.1.2-SNAPSHOT"]]
+  :plugins [[lein-cloverage "1.1.2"]]
   :cloverage {:ns-exclude-regex [#"user"
                                  #"benchmark"
                                  #"tarayo\.spec"]}
